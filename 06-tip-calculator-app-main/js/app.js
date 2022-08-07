befa920dbba5d6d55f955950d3b36310b55d67cb
MikeOdhiambo <odhiambomike413@gmail.com>
2022-08-07 22:06:55 +0300

    Add js and get elements

---
+++ 06-tip-calculator-app-main/js/app.js
@@ -0,0 +1,14 @@
+const bill = document.getElementById("bill")
+const noOfPple = document.getElementById("no-pple")
+const eachTip = document.getElementById("tip-amt")
+const eachTtl = document.getElementById("ttl-amt")
+const reset = document.getElementById("reset")
+const cstmPrcnt = document.getElementsByClassName("cstm")[0]
+const tipPrcnt = document.getElementsByClassName("tip-prcnt")
+
+
+window.onload = main()
+
+function main(){
+    
+}
